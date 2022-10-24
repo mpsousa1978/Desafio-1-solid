f75aaf4a58586ebcade3aa2c3fc06ecac6c64383
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2022-10-24 14:29:30 -0300

    Add files via upload

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,14 +1,15 @@
 import { User } from "../../model/User";
+
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
   private users: User[];
-
   private static INSTANCE: UsersRepository;
 
   private constructor() {
     this.users = [];
   }
+
 
   public static getInstance(): UsersRepository {
     if (!UsersRepository.INSTANCE) {
@@ -19,23 +20,38 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user,{
+                  name,
+                  email,
+                  created_at : new Date(),
+                  updated_at : new Date()
+                });
+
+    this.users.push(user);
+    return user;
   }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
+  //findById(id: string): User | undefined {
+    findById(id: string): User {
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
+  //findByEmail(email: string): User | undefined {
+  findByEmail(email: string): User  {
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    receivedUser.admin = true;
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
