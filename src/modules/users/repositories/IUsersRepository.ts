0e051e55f2441c9a1731a61c60f82c4db99059b2
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2022-10-24 18:12:00 -0300

    Add files via upload

---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,17 @@
+import { User } from "../model/User";
+
+interface ICreateUserDTO {
+  name: string;
+  email: string;
+}
+
+interface IUsersRepository {
+  create({ name, email }: ICreateUserDTO): User;
+  findById(id: string): User;
+  findByEmail(email: string): User;
+  turnAdmin(user: User): User;
+  list(): User[];
+  
+}
+
+export { IUsersRepository, ICreateUserDTO };
