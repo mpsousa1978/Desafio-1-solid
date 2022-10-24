b549bd98e8b72d0e140562b275db685f526aaeb0
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2022-10-24 14:26:49 -0300

    Add files via upload

---
+++ src/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const {user_id} = request.body;
+    const all = this.listAllUsersUseCase.execute(user_id);
+    return response.json(all);
+  }
+}
+
+export { ListAllUsersController };
