7edb02f9d629516efe0514de2b5c0c6d028c3d08
marcio pereira de sousa <114409309+mpsousa1978@users.noreply.github.com>
2022-10-25 08:25:44 -0300

    Add files via upload

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const {user_id} = request.body;
+    const {user_id}:any = request.headers;
 
     try{
       const users = this.listAllUsersUseCase.execute({user_id});
       return response.status(201).json(users);
+
     } catch (error) {
       return response.status(400).json({error:error.message});
     }
